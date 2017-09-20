96336bc70c2948f9b379bd9a7d741b2705c7daf7
flozoswoops <flores.a.mark@gmail.com>
2017-09-19 21:46:48 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express()
 
+app.get("/", (req,res) => {
+    res.send("Welcome to pizza Express")
+});
+
+app.get("/toppings/:type", (req,res) => {
+    res.send(`${req.params.type} pizza! Good choice!`)
+})
+
+app.get("/order/:amount/:size", (req,res) => {
+    res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`)
+
+});
+
 var port = 3000
 
 app.listen (port, () => {
