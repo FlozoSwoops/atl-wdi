fb682130136b408a30426a023bd2c02a0fee30f6
flozoswoops <flores.a.mark@gmail.com>
2017-10-06 09:07:57 -0400

    halfway point

--- homework/express/express-pirates-update-delete/controllers/pirates.js
+++ homework/express/express-pirates-update-delete/controllers/pirates.js
@@ -41,13 +41,37 @@ router.get('/:id', function(req, res){
 //==============================
 // UPDATE
 //==============================
-
+router.get('/:pirateid/edit', (request, response) => {
+	res.render('pirates/edit', {
+		pirates: {
+		  id: req.params.id,
+		  name: pirates.id.name,
+		  birthplace: pirates.id.birthplace,
+		  death_year: pirates.id.death_year,
+		  base: pirates.id.base,
+		  nickname: pirates.id.nickname
+		}
+	  });
+	  then(() =>{
+		  ressponse.redirect('/pirates')
+	  })
+}
+)
 //==============================
 // DESTROY
 //==============================
-
+router.get('/:id/delete', (request, response) => {
+	
+	pirates.splice(req.params.id, 1);
+	 then(() => {
+		 response.render(/pirates)
+	 })
+	
+		
 //==============================
 // EXPORTS
 //==============================
 
 module.exports = router;
+name
+
