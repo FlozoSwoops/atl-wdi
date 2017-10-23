e3c189184f1f600a6a1d26d92d4e00571d91da20
flozoswoops <flores.a.mark@gmail.com>
2017-10-23 16:05:57 -0400

    boolean skills

---
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -0,0 +1,15 @@
+1. Vatican
+
+2. SELECT * FROM countrylanguage WHERE countrycode='VAT'
+italian
+
+3.SELECT * FROM countrylanguage WHERE language = 'Italian'
+san marino
+
+4.SELECT name FROM city WHERE countrycode = 'SMR';
+serravalle
+
+5.SELECT name FROM city WHERE id = '211'
+serra, brazil
+
+6.SELECT name FROM country WHERE code = 'BRA
