8c03a71186444e3077673fe71a338acea23d19bb
flozoswoops <flores.a.mark@gmail.com>
2017-10-29 21:16:20 -0400

    part 5

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -1,29 +1,46 @@
 console.log('tamagotchi file is loaded');
 
 class Tamagotchi {
-    constructor(name, creatureType){
-      this.foodInTummy = 10;
-      this.restedness = 10;
-      this.health = 10;
-      this.name = name;
-      this.creatureType = creatureType;
+    constructor(name, creatureType) {
+        this.foodInTummy = 10;
+        this.restedness = 10;
+        this.health = 10;
+        this.name = name;
+        this.creatureType = creatureType;
     }
 
-cry() {
-    foodInTummy -= 1,
-    console.log(this.name + this.foodInTummy)
-    console.log("Waahhh")
-}
-puke() {
-    foodInTummy --
-    console.log("food: " + this.foodInTummy = "Wah")
-}
+    cry() {
+        foodInTummy -= 1,
+            console.log(this.name + this.foodInTummy)
+        console.log("Waahhh")
+    }
+    puke() {
+        foodInTummy--
+        console.log("food: " + this.foodInTummy = "Wah")
+    }
 
-yawn() {
-    restedness --
-    console.log(this.name + "has current restedness of:" + this.restedness)
-}
+    yawn() {
+        restedness--
+        console.log(this.name + "has current restedness of:" + this.restedness)
+    }
 
+    start() {
+        this.timerOne = setInterval(function () {
+            this.cry();
+        }, 6000);
+        this.timerTwo = setInterval(function () {
+            this.yawn();
+        }, 10000);
+        this.timerThree = setInterval(function () {
+            this.puke();
+        }, 20000);
+    };
+    stop() {
+        clearInterval(this.timerOne)
+        clearInterval(this.timerTwo)
+        clearInterval(this.timerThree)
+    }
+}
 
 //from stuck section
 var p1 = new Tamagotchi('Mark, shmoo');
