ac470b29cbe13d9017b29cc80d9932d3f5a8cfb6
flozoswoops <flores.a.mark@gmail.com>
2017-10-29 21:05:26 -0400

    part 4 probably not right

--- homework/javaScript/js-constructor/player.js
+++ homework/javaScript/js-constructor/player.js
@@ -4,14 +4,17 @@ var player = {
   name: "Neo",
   feedTamagotchi: function(tamagotchi) {
     tamagotchi.foodInTummy++;
+    p1.feedTamagotchi(p1)
     console.log('YUM!!! ' + tamagotchi.name + ' has been fed and now has: ' + tamagotchi.foodInTummy + ' in the tummy');
   },
   medicateTamagotchi: function(tamagotchi) {
     tamagotchi.health++;
+    p1.medicateTamagotchi()
     console.log(tamagotchi.name + ' has been medicated and now has health: ' + tamagotchi.health);
   },
   knockOutTamagotchi: function(tamagotchi) {
     tamagotchi.restedness++;
+    p1.knockOutTamagotchi()
     console.log(tamagotchi.name + ' got some Zzzzs! Restedness: ' + tamagotchi.restedness);
   }
 }; //end player
